6f88be4cfbc55d0e8a61824c50c486dd33715ce5
LiuYongJie <1058433796@qq.com>
2022-03-15 15:28:48 +0800

    modified README.txt

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
 一个用于实体识别的Bi-LSTM-CRF模型
+数据集： source_BIO_2014_cropus.txt & target_BIO_2014_cropus.txt
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from myModel import *
 from myDataset import *
 from tqdm import tqdm, trange
